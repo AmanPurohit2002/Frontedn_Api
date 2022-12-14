35b7b1efc9549dce4ec9d4d2baef3140d5bef464
Aman_Purohit <amanpurohit785@gmail.com>
2022-12-15 01:17:52 +0530

    search bar is in between

---
+++ src/api.js
@@ -0,0 +1,27 @@
+const fetchData=async(term)=>{
+    const response=await fetch('https://api.unsplash.com/search/photos?'+new URLSearchParams({
+        query:term
+    }).toString(),{
+        method:'GET',
+        headers:{
+            Authorization: 'Client-ID zniY3XYEmc9n3EpignuGOWA4ZxFFeUtUXS5vIIMukPU'
+        } 
+        //post request 
+        // body:JSON.stringify({
+        //     query:'cars'
+        // })
+    })
+
+    const json=await response.json(); //return the whole body  of http response
+
+    const data=json.results;
+
+    console.log(data);
+    
+    return json;
+}
+
+// fetchData();
+
+
+export default fetchData; 
